ecaf8d94adddbfb4824fed9089b932f103b74a15
Скоробогатый Евгений <evskorobogatij@gmail.com>
2021-02-15 16:54:28 +0300

    Список типов компьютеров

---
+++ src/Controller/Api/TypeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\Helpers\TypeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class TypeController
+ * @package App\Controller\Api
+ * @Route("/api/type")
+ */
+class TypeController extends AbstractController
+{
+    /**
+     * @Route("/", name="api_type_list", methods={"GET"})
+     */
+    public function index(TypeRepository $typeRepository): Response
+    {
+        $types = $typeRepository->findAll();
+        return $this->json($types);
+    }
+}
