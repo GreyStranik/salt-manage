8af33fab9eb06a5905dc032da86713181a3e18dd
Скоробогатый Евгений <evskorobogatij@gmail.com>
2021-02-16 09:05:12 +0300

    Список установленных операционных систем

---
+++ src/Controller/Api/OsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\Helpers\OsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class OsController
+ * @package App\Controller\Api
+ * @Route("/api/os")
+ */
+class OsController extends AbstractController
+{
+    /**
+     * @Route("/", name="api_os_list", methods={"GET"})
+     */
+    public function index(OsRepository $osRepository): Response
+    {
+        $data = $osRepository->findAll();
+        $oss = [];
+        foreach ($data as $item){
+            $oss[] = [
+                'id' => $item->getId(),
+                'name' => $item->getName()
+            ];
+        }
+        return $this->json($oss);
+    }
+}
