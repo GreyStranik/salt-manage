5780762fcda967a5f568b0c40d703897ad483cd9
Скоробогатый Евгений <evskorobogatij@gmail.com>
2021-03-05 14:43:59 +0300

    Обновления в списке миньенов

--- assets/pages/Minions.tsx
+++ assets/pages/Minions.tsx
@@ -6,37 +6,60 @@ import {renderCellExpand} from "../components/addons/GridCellExpand"
 import CustomGridPagination from "../components/addons/CustomGridPagination";
 import NoDataOverlay from "../components/addons/NoDataOverlay";
 
-const columns: ColDef[] = [
-    { field: 'node_name', headerName: 'Компьютер', flex: 1, renderCell: renderCellExpand },
-    { field: 'selialnumber', headerName: 'Серийный номер', width: 180, renderCell: renderCellExpand },
-    { field: 'ip', headerName: 'IP адрес', width: 210, renderCell: renderCellExpand},
-    { field: 'mac', headerName: 'MAC адрес', width: 210, renderCell: renderCellExpand},
-    { field: 'fio_user', headerName: 'ФИО ответственного', flex: 1, renderCell: renderCellExpand},
-    { field: 'user_phone', headerName: 'Телефон', width: 200, renderCell: renderCellExpand},
-    { field: 'room', headerName: 'Кабинет', width: 180, renderCell: renderCellExpand},
-    {
-        field: 'created_at',
-        headerName: 'Создано',
-        width: 180,
-        type:"dateTime",
-        hide:true,
-        valueFormatter: params => new Date(params.value as string).toLocaleString(),
-    },
-    {
-        field: 'updated_at',
-        headerName: 'Обновлено',
-        width: 180,
-        type:"dateTime",
-        valueFormatter: params => new Date(params.value as string).toLocaleString(),
+import {NavLink , useLocation} from 'react-router-dom';
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
 
-    }
-];
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        table_cell_link : {
+            textDecoration : 'none',
+            color : theme.palette.text.primary
+        }
+    }),
+);
 
 function Minions() {
+    const classes = useStyles()
 
     const [minions,setMinions] = useState<RowsProp>([])
 
     const [loading, setLoading] = useState(true)
+
+    const columns: ColDef[] = [
+        { field: 'node_name', headerName: 'Компьютер', flex: 1,
+            renderCell: params => {
+                console.log(params.row.id)
+                return  (
+                    <NavLink to={`/minions/${params.row.id}`} className={classes.table_cell_link} >
+                        {params.value}
+                    </NavLink>
+                )
+            }
+        },
+        { field: 'selialnumber', headerName: 'Серийный номер', width: 180, renderCell: renderCellExpand },
+        { field: 'ip', headerName: 'IP адрес', width: 210, renderCell: renderCellExpand},
+        { field: 'mac', headerName: 'MAC адрес', width: 210, renderCell: renderCellExpand},
+        { field: 'fio_user', headerName: 'ФИО ответственного', flex: 1, renderCell: renderCellExpand},
+        { field: 'user_phone', headerName: 'Телефон', width: 200, renderCell: renderCellExpand},
+        { field: 'room', headerName: 'Кабинет', width: 180, renderCell: renderCellExpand},
+        {
+            field: 'created_at',
+            headerName: 'Создано',
+            width: 180,
+            type:"dateTime",
+            hide:true,
+            valueFormatter: params => new Date(params.value as string).toLocaleString(),
+        },
+        {
+            field: 'updated_at',
+            headerName: 'Обновлено',
+            width: 180,
+            type:"dateTime",
+            valueFormatter: params => new Date(params.value as string).toLocaleString(),
+
+        }
+    ];
+
 
     useEffect(()=>{
         setLoading(true);
