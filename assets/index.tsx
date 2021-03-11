584c760d650a62c0a5302dcadafeff672c6d75a2
Скоробогатый Евгений <evskorobogatij@gmail.com>
2021-03-11 11:53:40 +0300

    Изменения в шрифтах

--- assets/index.tsx
+++ assets/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router  } from "react-router-dom"
-
+import 'fontsource-roboto';
 import App from "./App";
 
 ReactDOM.render(
